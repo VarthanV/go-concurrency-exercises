37350ebb3bee375cf1b84925aa1c99afa34387c9
Vishnu Varthan <vishnulatha006@gmail.com>
2024-10-23 23:50:52 +0530

    add memory-profiling

--- memory-profiling/main.go
+++ memory-profiling/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
 
+/*
+A newly minted goroutine is given a few kilobytes,
+which is almost always enough. When it isn’t,
+the run-time grows (and shrinks) the memory for storing the stack automatically, allowing many goroutines to live in a modest amount of memory.
+*/
 func main() {
-	fmt.Println("hello world")
+
+	var wg sync.WaitGroup
+	var c <-chan interface{}
+
+	memConsumed := func() uint64 {
+		runtime.GC()
+		var s runtime.MemStats
+		runtime.ReadMemStats(&s)
+		return s.Sys
+	}
+
+	noop := func() {
+		wg.Done()
+		<-c
+	}
+
+	const numGoroutines = 1e4
+	wg.Add(numGoroutines)
+
+	before := memConsumed()
+
+	for i := 0; i < numGoroutines; i++ {
+		go noop()
+	}
+
+	wg.Wait()
+
+	after := memConsumed()
+
+	fmt.Printf("%.3fkb\n", float64(after-before)/numGoroutines/1000)
 }
